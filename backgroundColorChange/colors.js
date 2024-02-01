0bbc477aafdcefe1ff971268e2d4bcc861e53491
hament <hemantkumar2708@gmail.com>
2024-02-01 14:59:48 +0530

    change file name

--- backgroundColorChange/colors.js
+++ backgroundColorChange/colors.js
@@ -1,16 +1,12 @@
 var index = 0;
-
 function changeColors() {
     var colors = ["red", "blue", "yellow", "green", "orange", "black", "pink"]
     console.log(index)
     document.getElementsByTagName("body")[0].
         style.background = colors[index++];
-
         console.log(index)
     if (index > (colors.length - 1))
     {
         index = 0;
-
     }
-    console.log(index)
 }
