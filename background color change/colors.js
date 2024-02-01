bfa7af4838b262447dfcec8bf48c44d0675b5aea
hament <hemantkumar2708@gmail.com>
2024-02-01 14:52:16 +0530

    modified background color

--- background color change/colors.js
+++ background color change/colors.js
@@ -1,11 +1,16 @@
 var index = 0;
 
-function changeColors(){
-    var colors = ["red", "blue", "yello", "green","orange","black","pink"]
-
+function changeColors() {
+    var colors = ["red", "blue", "yellow", "green", "orange", "black", "pink"]
+    console.log(index)
     document.getElementsByTagName("body")[0].
-    style.background=colors[index++];
+        style.background = colors[index++];
 
-    if(index > colors.length - 1)
-    index=0;
+        console.log(index)
+    if (index > (colors.length - 1))
+    {
+        index = 0;
+
+    }
+    console.log(index)
 }
